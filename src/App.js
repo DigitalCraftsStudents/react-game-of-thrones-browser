1cb83f000d7d0cf8dcbccc9e5868d894edbf9280
Chris Aquino <chris@radishmouse.com>
2021-01-23 10:23:02 -0500

    shows List and Details components to implement character browser

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
+import List from './List';
+import Details from './Details';
+
+import {
+  getCharacterList,
+  getCharacter,
+} from './api';
+
+
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const [char, setChar] = useState(null);
+  const [page, setPage] = useState(1);
+  
+  async function retrieveCharacters() {
+    const charactrsFromApi = await getCharacterList(page);
+    setCharacters(charactrsFromApi);
+  }
+
+  function prev() {
+    setPage(page - 1);
+    setCharacters([]);
+    setChar(null);
+  }
+  function next() {
+    setPage(page + 1);
+    setCharacters([]);
+    setChar(null);
+  }
+  
+  async function chooseCharacter(url) {
+    const character = await getCharacter(url);
+    setChar(character);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { page > 1 ?
+               <button onClick={prev}>prev</button>
+        :
+               <button disabled onClick={prev}>prev</button>
+      }
+      <button onClick={retrieveCharacters}>
+        Get Page {page} of Characters
+      </button>    
+      <button onClick={next}>next</button>
+      { characters.length > 0 &&
+        <nav>
+          <List
+            characterArray={characters}
+            onChoice={chooseCharacter}
+          />
+        </nav>
+      }
+      { char &&
+        <Details character={char} />
+      }
     </div>
   );
 }
